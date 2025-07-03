8ded5213ad542e91985087d6f1662954ef01b25e
Nelson C. <ncdev2015@gmail.com>
2025-07-03 11:37:17 -0300

    chore: format code with consistent indentation

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -19,30 +19,29 @@ export default function Services() {
 		    	key={i}
 		    	className="flip-card h-82"
 		    >
-		     	<div className="flip-inner border border-gray-200 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-200 shadow-sm hover:shadow-md transition-all duration-300 w-full h-full relative">
+		     	<div className="flip-inner border border-gray-200 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-200 shadow-sm hover:shadow-md transition-all duration-300 w-full h-full relative min-h-0">
 		        
-		        {/* Front face */}
-		        <div className="flip-front p-6 h-full overflow-hidden flex flex-col select-none text-gray-800">
-		          <h3 className="text-xl font-semibold mb-2 text-blue-700">
-		            {i === 1 ? "Web Development" : i === 2 ? "Mobile Development" : "IT Training"}
-		          </h3>
-		          <p className="text-base overflow-auto">
-		            {i === 1 ? desc1 : i === 2 ? desc2 : desc3}
-		          </p>
-		        </div>
+			        {/* Front face */}
+			        <div className="flip-front p-6 h-full overflow-hidden flex flex-col select-none text-gray-800">
+			        	<h3 className="text-xl font-semibold mb-2 text-blue-700">
+			            	{i === 1 ? "Web Development" : i === 2 ? "Mobile Development" : "IT Training"}
+			        	</h3>
+			        	<p className="text-base overflow-auto">
+			        		{i === 1 ? desc1 : i === 2 ? desc2 : desc3}
+			        	</p>
+			        </div>
 
-		        {/* Back face */}
-		        <div className="flip-back bg-gray-800 text-gray-100 p-6 h-full overflow-hidden flex flex-col rounded-lg select-none">
-		          <h4 className="text-lg text-blue-500 font-semibold mb-2 select-none">More Info</h4>
-		          <p className="text-base overflow-auto">
-		            {i === 1
-		              ? "HTML, CSS, JS, React, Next.js, Tailwind..."
-		              : i === 2
-		              ? "React Native, Flutter, deployment tips..."
-		              : "Workshops, programming bootcamps, 1:1 training..."}
-		          </p>
-		        </div>
-		        
+		        	{/* Back face */}
+		        	<div className="flip-back bg-gray-800 text-gray-100 p-6 h-full overflow-hidden flex flex-col rounded-lg select-none">
+		          	<h4 className="text-lg text-blue-500 font-semibold mb-2 select-none">More Info</h4>
+		          	<p className="text-base overflow-auto">
+		            	{i === 1
+		              	? "HTML, CSS, JS, React, Next.js, Tailwind..."
+		              	: i === 2
+		              	? "React Native, Flutter, deployment tips..."
+		              	: "Workshops, programming bootcamps, 1:1 training..."}
+		          	</p>
+		        </div>		        
 		      </div>
 		    </div>
 		  ))}
